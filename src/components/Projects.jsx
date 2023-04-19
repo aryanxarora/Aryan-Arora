210270ec81ea60d2e3321c8c39772980aa569fb8
Aryan Arora <aryanxarora@gmail.com>
2023-04-19 13:42:01 +0000

    Tile, Props

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,30 @@ import Tile from "./Tile";
 function Projects() {
   return (
     <div className="lg:px-20 lg:flex lg:flex-wrap">
-      <Tile />
-      <Tile />
+      <Tile
+        imageUrl={
+          "https://drive.google.com/uc?export=view&id=13XgvKXnXB4neG4yXixqc-b9iaJch0roL"
+        }
+        title={
+          "HR MANAGEMENT SYSTEM TO KEEP TRACK OF TEACHERS AND STAFF PAYROLL"
+        }
+        tag={"Java -- Swing"}
+        description={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        }
+      />
+      <Tile
+        imageUrl={
+          "https://drive.google.com/uc?export=view&id=1pPjNyBmk4SzwczoO6qgqHpJuY3o8goUY"
+        }
+        title={
+          "WAREHOUSE INVENTORY MANAGEMENT DATABASE FOR LUXURY ONLINE RETAILER"
+        }
+        tag={"SQL -- Datagrip"}
+        description={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        }
+      />
     </div>
   );
 }
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,24 +1,20 @@
-function Tile() {
+function Tile({ imageUrl, title, tag, description }) {
   return (
     <div className="p-5 lg:flex lg:flex-wrap lg:w-2/4">
       {/* Image */}
-      <div className="pb-5 lg:pb-0 lg:basis-1/2 cursor-ne-resize">
-        <img src={require("../images/hr_system.png")} />
+      <div className="pb-5 lg:pb-0 lg:basis-1/2 cursor-alias">
+        <img src={imageUrl} alt="tile_image" />
       </div>
       {/* Body */}
       <div className="lg:basis-1/2 lg:pl-5">
         {/* Title */}
-        <p className="font-sans lg:text-2xl hover:underline cursor-ne-resize">
-          HR MANAGEMENT SYSTEM TO KEEP TRACK OF TEACHERS AND STAFF PAYROLL
+        <p className="font-sans lg:text-2xl hover:underline cursor-alias">
+          {title}
         </p>
         {/* Tag */}
-        <p className="font-serif text-base">Java -- Swing</p>
+        <p className="font-serif text-base">{tag}</p>
         {/* Description */}
-        <p className="font-serif lg:pt-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit
-          amet consectetur adipisicing elit.
-        </p>
+        <p className="font-serif lg:pt-10">{description}</p>
       </div>
     </div>
   );
