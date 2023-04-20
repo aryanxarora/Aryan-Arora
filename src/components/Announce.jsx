0dbee749fbaedf5183180212b314d101c1047326
aryanxarora <aryanxarora@gmail.com>
2023-04-20 00:01:36 -0400

    Minor Design Updates

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -2,8 +2,11 @@ function Announce() {
   return (
     <div className="bg-slate-200 px-5 py-3">
       <h1 className="text-center text-xs lg:text-sm">
-        🚀 Currently: Working on this portfolio website + actively searching for
-        a front-end dev role.
+        Actively searching for a front-end developer role, view my{" "}
+        <a href="#" className="text-blue-500 hover:text-blue-400">
+          resume
+        </a>
+        .
       </h1>
     </div>
   );
