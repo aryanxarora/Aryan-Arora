a3ce85777da6affb50dd619030aaf3bbdb65a93f
aryanxarora <aryanxarora@gmail.com>
2023-06-01 21:31:08 -0400

    Projects Content

--- src/models/projects.js
+++ src/models/projects.js
@@ -3,56 +3,56 @@ const projects = [
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/kitchenette.png",
     "title" : "RECIPE SHARING WEB APP",
     "tag" : "EJS, Express, MongoDB, NodeJS, Cookies",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "The name of this web app is Kitchenete. The story behind this is... I was living alone in an apartment near my university and so were my friends. We decided to share the recipes we were cooking. At the time, Notion wasn't really a big thing (something that could be used for this purpose) so I decided to create a project around it. A web app where you can upload recipes and share them with your friends!",
     "repo" : "https://github.com/aryanxarora/dlsu-kitchenette"
   },
   {
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/ergofix.png",
     "title" : "ERGOFIX PHILIPPINES ECOMMERCE STORE",
     "tag" : "HTML/CSS/JS",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "My good friend had started a business in the Philippines selling sit-stand desks, it was called Ergofix. He hired me to create an e-commerce website for his business. I designed this entire website in Adobe XD and developed it from scratch using HTML/CSS and JS.",
     "repo" : "https://ergofix.ph/"
   },
   {
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/hr_system.png",
     "title" : "HR MANAGEMENT SYSTEM TO KEEP TRACK OF TEACHERS AND STAFF PAYROLL",
     "tag" : "Java — Swing, OOP, Files",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "This is a java swing application that connects to a local postgresql database to store and display teachers and staff in the employee management system. It also calculates the payroll for all employees. The project used all OOP concepts.",
     "repo" : "https://github.com/aryanxarora/project-hr-management-system"
   },
   {
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/databases.png",
     "title" : "WAREHOUSE INVENTORY MANAGEMENT DATABASE",
     "tag" : "SQL — Datagrip, ERD",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "This project was done in fulfiment of a class, I designed a database that would be used by online retailers. The database had many tables to account for the products, colors, quantities, customer orders, locations in warehouse among others. I created an ERD using plantUML and then wrote CREATE and INSERT sql statements using postgresql in datagrip.",
     "repo" : "https://github.com/aryanxarora/project-hr-management-system"
   },
   {
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/alpha_phi.png",
     "title" : "ALPHA PHI BETA FRATERNITY WEBSITE",
     "tag" : "HTML/CSS/JS",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "My first ever freelance gig! and definitely my proudest one. I created this whole website using vanilla HTML and CSS. The scope of this website was very large for the skills I had back then when I made this website. Nonetheless, I was able to finish the whole project within timeframe and the client was very happy with the result.",
     "repo" : "https://alpha-phi-beta.netlify.app/index.html"
   },
   {
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/thesis.png",
     "title" : "AUTHENTICATION AND GROUP KEY MANAGEMENT PROTOCOL",
     "tag" : "Python — Hashing, Ellptic Curve Cryptography",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "The most difficult project I've done in my coding journey. This was my undergraduate thesis. I implement a lightweight authentication and key management protocol that is used by microcontrollers to securely communicate in remote areas. The efficiency of the protocol was key here due to the memory and power resource constraints of micro controllers.",
     "repo" : "https://github.com/aryanxarora/dlsu-thesis"
   },
   {
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/ai.png",
     "title" : "SMALL ARTIFICIAL INTELLIGENCE GAMES",
     "tag" : "Python — OOP",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "I started learning python and wanted to do something for fun. Created a gold miner game and tictactoe with 3 levels of difficulty. ",
     "repo" : "https://github.com/aryanxarora/dlsu-ai-projects"
   },
   {
     "imageUrl" : "https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/images/first.png",
     "title" : "MY FIRST EVER WEBSITE",
     "tag" : "HTML/CSS/JS, Firebase, Netlify",
-    "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "description" : "My first portfolio website I decided to create to show off my skills and progress in front-end development. Again, it was created using pure HTML and CSS but I decided to add a admin side to this that lets you add new fields because I was too lazy to keep opening the code to push new things. The website was connected to Firebase Firestore to store all the data.",
     "repo" : "https://aryanxarora.github.io/"
   },
   // {
