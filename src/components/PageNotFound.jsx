1b68f6b80e0490d34d672dcbfc0f28342a01cde9
aryanxarora <aryanxarora@gmail.com>
2024-01-05 22:38:31 -0500

    Vision

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -3,6 +3,8 @@ function PageNotFound() {
     <div>
       <h1 className="font-serif text-2xl text-center mt-20">
         404! Page Not Found
+        <br />
+        Or... Coming Soon!
       </h1>
     </div>
   );
