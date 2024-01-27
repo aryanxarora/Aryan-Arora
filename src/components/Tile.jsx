2e604f0ddd8eae0e06224a663041e6e9de6230a7
aryanxarora <aryanxarora@gmail.com>
2024-01-27 00:51:13 -0500

    Icon svgs

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,6 +1,6 @@
 function Tile({ imageUrl, title, tag, techstack, description, repo }) {
   return (
-    <div className="p-5 lg:flex lg:flex-wrap lg:w-2/4">
+    <div className="p-5 lg:flex lg:flex-wrap lg:w-2/4 mb-10">
       {/* Image */}
       <div className="pb-5 lg:pb-0 lg:basis-1/2 cursor-alias">
         <img src={imageUrl} alt="tile_image" />
@@ -14,13 +14,13 @@ function Tile({ imageUrl, title, tag, techstack, description, repo }) {
           </p>
         </a>
         {/* Tag */}
-        <p className="font-serif text-base">{tag}</p>
-        <div className="flex">
+        <p className="font-serif text-base mt-2">{tag}</p>
+        <div className="flex gap-3 mt-5">
           {techstack.map((item) => (
             <img
-              src={`https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/icons/${item}.png`}
+              src={`https://raw.githubusercontent.com/aryanxarora/Aryan-Arora/main/src/icons/${item}.svg`}
               alt=""
-              className="w-16"
+              className="w-7"
             />
           ))}
         </div>
